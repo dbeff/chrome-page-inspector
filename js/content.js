9e8ae309d697a5e71ebac83519addbf1d32d5877
Daniel Beff <danielbeff@MacBook-Pro-de-Daniel.local>
2015-02-17 02:08:12 -0200

    refactor data object structure and integrate with angular

--- js/content.js
+++ js/content.js
@@ -1,32 +1,81 @@
 console.log ('frontendler inspector | content.js');
-var dados = {};
+
+function Content (){
+	this.data = {};
+	this.data.meta = {};
+	this.data.meta.title = $('title').html();
+	this.data.meta.description = $("meta[name='description']").attr('content');
+	this.data.meta.keywords = $("meta[name='keywords']").attr('content');
+	this.data.meta.viewport = $("meta[name='viewport']").attr('content');
+
+	this.data.social = {};
+	this.data.social.ogTitle = $("meta[property='og:title']").attr('content');
+	this.data.social.ogDescription = $("meta[property='og:description']").attr('content');
+	this.data.social.ogUrl = $("meta[property='og:url']").attr('content');
+	this.data.social.ogSiteName = $("meta[property='og:site_name']").attr('content');
+	this.data.social.ogImage = $("meta[property='og:image']").attr('content');
+	this.data.social.ogLocale = $("meta[property='og:locale']").attr('content');
+	this.data.social.ogType = $("meta[property='og:type']").attr('content');
+
+	this.data.social.twitterCard = $("meta[name='twitter:card']").attr('content');
+	this.data.social.twitterSite = $("meta[name='twitter:site']").attr('content');
+	this.data.social.twitterTitle = $("meta[name='twitter:title']").attr('content');
+	this.data.social.twitterDescription = $("meta[name='twitter:description']").attr('content');
+	this.data.social.twitterImage = $("meta[name='twitter:image']").attr('content');
+	this.data.social.twitterUrl = $("meta[name='twitter:url']").attr('content');
+
+	this.data.favicon = {};
+	this.data.favicon.msTileImage =
+	this.data.favicon.msTileColor = '';
+	this.data.favicon.msTileColor = '';
+
+	this.data.style = {};
+	this.data.script = '';
+	this.data.style.links = [];
+
+	this.data.headers = '';
 
 
-function getHeaders (){
-	var req = new XMLHttpRequest();
-	req.open('GET', document.location, false);
-	req.send(null);
-	var headers = req.getAllResponseHeaders().toLowerCase();
-	return headers;
+	this.getStyles = function (){
+		var elements = document.querySelectorAll("link[rel='stylesheet']");
+		for (var i = 0; i < elements.length; i++){
+			this.data.style.links.push(elements[i].getAttribute('href'));
+		}
+	};
+
+	this.getHeaders = function (){
+		var req = new XMLHttpRequest();
+		req.open('GET', document.location, false);
+		req.send(null);
+		this.data.headers = req.getAllResponseHeaders().toLowerCase();
+	};
+
+	this.getHeader = function (name){
+		var req = new XMLHttpRequest();
+		req.open('GET', document.location, false);
+		req.send(null);
+		var header = req.getResponseHeader(name).toLowerCase();
+		return header;
+	};
+
+
+	this.chromeOnRquest = function (){
+		var dataJson = JSON.stringify(this.data);
+		chrome.extension.onRequest.addListener(
+			function(request, sender, sendResponse) {
+				sendResponse (dataJson);
+			}
+		);
+	};
+
+	this.getStyles();
+	this.getHeaders();
+	this.chromeOnRquest();
 }
 
 
-function getData (){
-    //HTML
-    dados.title = $('title').html();
-    dados.description = $("meta[name='description']").attr('content');
-    dados.viewport = $("meta[name='viewport']").attr('content');
+new Content();
 
-    //META
-    dados.ogTitle = $("meta[property='og:title']").attr('content');
-    dados.ogImage = $("meta[property='og:image']").attr('content');
-	dados.headers = getHeaders();
-    console.log(dados);
-    return JSON.stringify(dados);
-}
 
-chrome.extension.onRequest.addListener(
-    function(request, sender, sendResponse) {
-        sendResponse (getData());
-    }
-);
+
+
--- js/popup.js
+++ js/popup.js
@@ -1,33 +1,35 @@
 (function(){
 	console.log ('frontendler inspector | popup.js');
+
+	var data = {};
 	var app = angular.module('inspector',[]);
-	var dados = {};
 
 	app.controller('PageController',function(){
 		this.current = 'seo';
-		console.log ('page: ' + this.current);
 		this.goTo = function (page){
 			page = page || 'seo';
 			this.current = page;
-			console.log ('page: ' + this.current);
 		};
 	});
 
-	//chrome.tabs.getSelected(null,function(tab) {
-	//	chrome.tabs.sendRequest(tab.id, 'mensagem', function(response) {
-	//			console.log (response);
-	//			dados = JSON.parse(response);
-	//			console.log (dados);
-	//			$('.title').html("" + dados.title);
-	//			$('.description').html("" + dados.description);
-	//			$('.viewport').html("" + dados.viewport);
-	//			$('.og-title').html("" + dados.ogTitle);
-	//			$('.og-image').html("" + dados.ogImage);
-	//		}
-	//	);
-	//});
+	app.controller('ContentController',function(){
+		this.data = data;
+	});
 
-})()
+})();
 
+
+
+chrome.tabs.getSelected(null,function(tab) {
+	chrome.tabs.sendRequest(tab.id, 'mensagem', function(response) {
+		data = JSON.parse(response);
+
+		//update data in ContentController
+		var scope = angular.element($("main")).scope();
+		scope.$apply(function(){
+			scope.data = data;
+		});
+	});
+});
 
 
